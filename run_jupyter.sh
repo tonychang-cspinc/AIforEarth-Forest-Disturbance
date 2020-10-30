cb901b0dc2c83b69d9fe6d9855c7ccc69c4dff81
Ubuntu <cspadmin@daymet-zarr.rv1d5qvrxwjetghijzmxy23whd.bx.internal.cloudapp.net>
2020-10-30 17:12:25 +0000

    MOAR notebooks

--- run_jupyter.sh
+++ run_jupyter.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PORT=8080
+PORT=8765
 IP=0.0.0.0
 sh ./blobfuse_container.sh 
 jupyter notebook --port $PORT --ip $IP --no-browser --allow-root
--- start_container.sh
+++ start_container.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 IMAGENAME=cspinc/fia:sample-building
-DATADRIVE=/home/tony/datablob
-SECRETS=/home/tony/azure_secrets.env
-PORT=8080
+DATADRIVE=/home/cspadmin/datablob
+SECRETS=/home/cspadmin/azure_secrets.env
+PORT=8765
 #-v $DATADRIVE:/datadrive \
 docker run -it --rm --privileged \
     -p $PORT:$PORT \
