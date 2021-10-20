8fe1a7e93a75577e00d582d2fc67054b5a730b11
root <root@fia.rv1d5qvrxwjetghijzmxy23whd.bx.internal.cloudapp.net>
2021-10-20 12:43:45 -1000

    evalidator code

--- run_jupyter.sh
+++ run_jupyter.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PORT=8080
+PORT=8888
 IP=0.0.0.0
 sh ./blobfuse_container.sh 
 jupyter notebook --port $PORT --ip $IP --no-browser --allow-root
--- start_container.sh
+++ start_container.sh
@@ -3,12 +3,12 @@ USER=tony
 IMAGENAME=cspinc/fia:sample-building
 DATADRIVE=/home/$USER/datablob
 SECRETS=/home/$USER/azure_secrets.env
-PORT=8080
-#-v $DATADRIVE:/datadrive \
+PORT=8888
 docker run -it --rm --privileged \
     -p $PORT:$PORT \
     -v $('pwd'):/content \
     -w /content \
+    -v /datadrive:/datadrive \
     --env-file $SECRETS \
     $IMAGENAME \
     /bin/bash
